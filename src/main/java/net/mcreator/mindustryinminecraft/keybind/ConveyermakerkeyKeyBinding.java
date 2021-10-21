918d6e0eaa273e9fda665a37c9297b3a510afe6d
YeetLand-1 <lalwanim0101@gmail.com>
2021-10-20 20:45:01 -0500

    Framework for adding crafting recipies

---
+++ src/main/java/net/mcreator/mindustryinminecraft/keybind/ConveyermakerkeyKeyBinding.java
@@ -0,0 +1,104 @@
+
+package net.mcreator.mindustryinminecraft.keybind;
+
+import org.lwjgl.glfw.GLFW;
+
+import net.minecraftforge.fml.network.NetworkEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.client.Minecraft;
+
+import net.mcreator.mindustryinminecraft.procedures.ConveyermakerkeyOnKeyPressedProcedure;
+import net.mcreator.mindustryinminecraft.MindustryinminecraftModElements;
+import net.mcreator.mindustryinminecraft.MindustryinminecraftMod;
+
+import java.util.function.Supplier;
+import java.util.Map;
+import java.util.HashMap;
+
+@MindustryinminecraftModElements.ModElement.Tag
+public class ConveyermakerkeyKeyBinding extends MindustryinminecraftModElements.ModElement {
+	@OnlyIn(Dist.CLIENT)
+	private KeyBinding keys;
+	public ConveyermakerkeyKeyBinding(MindustryinminecraftModElements instance) {
+		super(instance, 28);
+		elements.addNetworkMessage(KeyBindingPressedMessage.class, KeyBindingPressedMessage::buffer, KeyBindingPressedMessage::new,
+				KeyBindingPressedMessage::handler);
+	}
+
+	@Override
+	@OnlyIn(Dist.CLIENT)
+	public void initElements() {
+		keys = new KeyBinding("key.mindustryinminecraft.conveyermakerkey", GLFW.GLFW_KEY_C, "key.categories.inventory");
+		ClientRegistry.registerKeyBinding(keys);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@SubscribeEvent
+	@OnlyIn(Dist.CLIENT)
+	public void onKeyInput(InputEvent.KeyInputEvent event) {
+		if (Minecraft.getInstance().currentScreen == null) {
+			if (event.getKey() == keys.getKey().getKeyCode()) {
+				if (event.getAction() == GLFW.GLFW_PRESS) {
+					MindustryinminecraftMod.PACKET_HANDLER.sendToServer(new KeyBindingPressedMessage(0, 0));
+					pressAction(Minecraft.getInstance().player, 0, 0);
+				}
+			}
+		}
+	}
+	public static class KeyBindingPressedMessage {
+		int type, pressedms;
+		public KeyBindingPressedMessage(int type, int pressedms) {
+			this.type = type;
+			this.pressedms = pressedms;
+		}
+
+		public KeyBindingPressedMessage(PacketBuffer buffer) {
+			this.type = buffer.readInt();
+			this.pressedms = buffer.readInt();
+		}
+
+		public static void buffer(KeyBindingPressedMessage message, PacketBuffer buffer) {
+			buffer.writeInt(message.type);
+			buffer.writeInt(message.pressedms);
+		}
+
+		public static void handler(KeyBindingPressedMessage message, Supplier<NetworkEvent.Context> contextSupplier) {
+			NetworkEvent.Context context = contextSupplier.get();
+			context.enqueueWork(() -> {
+				pressAction(context.getSender(), message.type, message.pressedms);
+			});
+			context.setPacketHandled(true);
+		}
+	}
+	private static void pressAction(PlayerEntity entity, int type, int pressedms) {
+		World world = entity.world;
+		double x = entity.getPosX();
+		double y = entity.getPosY();
+		double z = entity.getPosZ();
+		// security measure to prevent arbitrary chunk generation
+		if (!world.isBlockLoaded(new BlockPos(x, y, z)))
+			return;
+		if (type == 0) {
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("entity", entity);
+				$_dependencies.put("x", x);
+				$_dependencies.put("y", y);
+				$_dependencies.put("z", z);
+				$_dependencies.put("world", world);
+				ConveyermakerkeyOnKeyPressedProcedure.executeProcedure($_dependencies);
+			}
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/mindustryinminecraft/procedures/ConveyermakerkeyOnKeyPressedProcedure.java
@@ -0,0 +1,74 @@
+package net.mcreator.mindustryinminecraft.procedures;
+
+import net.minecraftforge.fml.network.NetworkHooks;
+
+import net.minecraft.world.IWorld;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.network.PacketBuffer;
+import net.minecraft.inventory.container.INamedContainerProvider;
+import net.minecraft.inventory.container.Container;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.Entity;
+
+import net.mcreator.mindustryinminecraft.gui.CraftingScreenGui;
+import net.mcreator.mindustryinminecraft.MindustryinminecraftMod;
+
+import java.util.Map;
+
+import io.netty.buffer.Unpooled;
+
+public class ConveyermakerkeyOnKeyPressedProcedure {
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				MindustryinminecraftMod.LOGGER.warn("Failed to load dependency entity for procedure ConveyermakerkeyOnKeyPressed!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				MindustryinminecraftMod.LOGGER.warn("Failed to load dependency x for procedure ConveyermakerkeyOnKeyPressed!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				MindustryinminecraftMod.LOGGER.warn("Failed to load dependency y for procedure ConveyermakerkeyOnKeyPressed!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				MindustryinminecraftMod.LOGGER.warn("Failed to load dependency z for procedure ConveyermakerkeyOnKeyPressed!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				MindustryinminecraftMod.LOGGER.warn("Failed to load dependency world for procedure ConveyermakerkeyOnKeyPressed!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		{
+			Entity _ent = entity;
+			if (_ent instanceof ServerPlayerEntity) {
+				BlockPos _bpos = new BlockPos((int) x, (int) y, (int) z);
+				NetworkHooks.openGui((ServerPlayerEntity) _ent, new INamedContainerProvider() {
+					@Override
+					public ITextComponent getDisplayName() {
+						return new StringTextComponent("CraftingScreen");
+					}
+
+					@Override
+					public Container createMenu(int id, PlayerInventory inventory, PlayerEntity player) {
+						return new CraftingScreenGui.GuiContainerMod(id, inventory, new PacketBuffer(Unpooled.buffer()).writeBlockPos(_bpos));
+					}
+				}, _bpos);
+			}
+		}
+	}
+}
